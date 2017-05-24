00d907fea08e1280ebcb9f8974ebef1230c27fd4
jlave2 <jlave2@uic.edu>
2017-05-23 23:59:57 -0500

    Finished databinding

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core'
+import { Component, OnInit, Output, EventEmitter } from '@angular/core'
 import { Recipe } from '../recipe.model'
 
 @Component({
@@ -7,8 +7,10 @@ import { Recipe } from '../recipe.model'
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>()
   recipes: Recipe[] = [
-    new Recipe('Kung Pao Chicken', 'This is a test', 'http://cf2.foodista.com/sites/default/files/styles/featured/public/field/image/chicken_24.jpg')
+    new Recipe('Kung Pao Chicken', 'Delicious spicy Chinese dish', 'http://i.imgur.com/vuq0WzG.jpg'),
+    new Recipe('Grilled Cayenne Shrimp', 'Scrumptious seafood!', 'http://i.imgur.com/QScRvNf.jpg')
   ]
 
   constructor() { }
@@ -16,4 +18,7 @@ export class RecipeListComponent implements OnInit {
   ngOnInit() {
   }
 
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe)
+  }
 }
